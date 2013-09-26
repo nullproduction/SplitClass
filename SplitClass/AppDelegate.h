2ff1fc11bc32ddeb25e330754ba07c8ad02ce8c9
nullproduction <nullproduction@gmail.com>
2013-09-26 18:11:19 +0400

    Initial Commit

---
+++ SplitClass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SplitClass
+//
+//  Created by mezhevikin on 26.09.13.
+//  Copyright (c) 2013 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SplitClass/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SplitClass
+//
+//  Created by mezhevikin on 26.09.13.
+//  Copyright (c) 2013 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
